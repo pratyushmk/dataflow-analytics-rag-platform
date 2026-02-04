ba1696f0ba686bd1d1ee1edabee5c5de3e3653d9
Pratyush Mulpuru <mk.pratyush@gmail.com>
2026-02-04 16:07:33 -0500

    Support API + Localstack AWS connection based on the environment it's hosted in like container vs running locally

--- utils/get_s3_fs.py
+++ utils/get_s3_fs.py
@@ -1,9 +1,11 @@
+import os
 import s3fs
 
 def get_s3_fs():
+    endpoint_url = os.getenv("AWS_ENDPOINT_URL", "http://localhost:4566")
     return s3fs.S3FileSystem(
         key="test",
         secret="test",
-        client_kwargs={"endpoint_url": "http://localhost:4566"},
+        client_kwargs={"endpoint_url": endpoint_url},
         config_kwargs={"s3": {"addressing_style": "path"}},
     )
